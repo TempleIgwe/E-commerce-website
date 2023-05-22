151d832c3d7b06bf1553781e97ed3a786620cce0
Temple Igwe <temple.igw@gmail.com>
2023-05-22 16:29:03 +0100

    Add media query for shop page

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+
+let mainImg = document.getElementById("main-image")
+let smallImage = document.getElementsByClassName("small-img")
+
+smallImage[0].onclick = () => {
+    mainImg.src = smallImage[0].src;
+}
+smallImage[1].onclick = () => {
+    mainImg.src = smallImage[1].src;
+}
+smallImage[2].onclick = () => {
+    mainImg.src = smallImage[2].src;
+}
+smallImage[3].onclick = () => {
+    mainImg.src = smallImage[3].src;
+}
+
